e6fffa7370caace8b28b1b0fcc5702f237ade7ca
bitdiskdev <bitdiskdev@qq.com>
2021-12-28 16:49:33 +0800

    'commit'

--- BtdToken.sol
+++ BtdToken.sol
@@ -141,6 +141,7 @@ contract BtdToken is BasicToken,Freeze {
      */
     function transferFrom(address sender, address recipient, uint256 amount) external override (BasicToken) returns (bool) {
         require(amount <= _allowances[sender][msg.sender]);
+        require(!freezes[msg.sender]);
         _transfer(sender, recipient, amount);
         _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
         return true;
@@ -200,8 +201,7 @@ contract BtdToken is BasicToken,Freeze {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
         require(amount <= _balances[sender]);
-        require(!_freezes[sender]);
-
+        require(!freezes[sender]);
 
         _balances[sender] = _balances[sender].sub(amount);
         _balances[recipient] = _balances[recipient].add(amount);
--- Freeze.sol
+++ Freeze.sol
@@ -3,22 +3,22 @@ pragma solidity ^0.7.0;
 import "./Ownable.sol";
 contract Freeze is Ownable{
 
-    mapping (address => bool) public _freezes;
+    mapping (address => bool) public freezes;
 
     function freezeOf(address account) public view returns (bool) {
-        return _freezes[account];
+        return freezes[account];
     }
 
-    function _freeze(address account)  public onlyOwner {
+    function freeze(address account)  public onlyOwner {
         require(account != address(0), "ERC20: freeze account the zero address");
 
-        _freezes[account] = true;
+        freezes[account] = true;
     }
 
 
-    function _unfreeze(address account)  public onlyOwner {
+    function unfreeze(address account)  public onlyOwner {
         require(account != address(0), "ERC20: freeze account the zero address");
 
-        _freezes[account] = false;
+        freezes[account] = false;
     }
 }
